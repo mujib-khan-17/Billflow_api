d6ee4a36215ccc96d996d8a16649d27c30db06bf
mujib-khan-17 <mujibmk17@gmail.com>
2026-04-08 16:44:28 +0530

    fix: update auth file

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -3,12 +3,17 @@ from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from app.schemas.auth import LoginRequest, TokenResponse
 from app.database import SessionLocal, get_db
+from app.schemas.user import UserCreate
 from app.utils.auth import verify_password
 from app.utils.jwt import create_access_token
 from app.crud.auth import get_user_by_email
 from app.utils.config import settings
+from app.models.user import User
+from passlib.context import CryptContext
 
 router = APIRouter()
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 @router.post("/login", response_model=TokenResponse)
 def login(request: LoginRequest, db: Session = Depends(get_db)):
@@ -35,3 +40,18 @@ def login_oauth2(form_data: OAuth2PasswordRequestForm = Depends(), db: Session =
     token_data = {"sub": str(user.id), "email": user.email_id, "role": user.role.value}
     access_token = create_access_token(token_data)
     return TokenResponse(access_token=access_token)
+
+@router.post("/register")
+def register(user: UserCreate, db: Session = Depends(get_db)):
+    hashed_password = pwd_context.hash(user.password)
+
+    new_user = User(
+        email=user.email,
+        password=hashed_password
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {"message": "User created successfully"}
